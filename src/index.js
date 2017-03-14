0e4d5d9c29e2da76d43e30cf3bc2a54e811b1007
Louis Dalligos <zindex14@gmail.com>
2017-03-14 15:04:10 +0800

    Added youtube api search package

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyAjcWggSGS67PSbVaViosMua6naSvRg2_Q';
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
+  console.log(data);
+});
 
 const App = function() {
   return (
@@ -12,6 +15,5 @@ const App = function() {
     </div>
   );
 };
-
 
 ReactDOM.render(<App />, document.querySelector('.container'));
