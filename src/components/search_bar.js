38f75dee4f2a003e1be4365d4dd6aedcfd9f4604
Louis Dalligos <zindex14@gmail.com>
2017-03-16 14:26:15 +0800

    Implement search functionality

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -15,14 +15,17 @@ class SearchBar extends Component {
     // return <input onChange={this.onInputChange} />;
     return (
       <div className="search-bar">
-        <input onChange={event => this.setState({ term: event.target.value })} />
+        <input
+          value={this.state.term}
+          onChange={event => this.onInputChange(event.target.value)} />
       </div>
     );
   }
 
-  // onInputChange(event) {
-  //   console.log(event.target.value);
-  // }
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
+  }
 }
 
 export default SearchBar;
